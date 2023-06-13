d67e33e4bb37c732b0545a6df6e72adbf8a22946
Wanton Raven <josephngalu96@gmail.com>
2023-06-13 11:42:28 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import "../styles/about.css";
 import { ContextProvider } from '../context'
 
 export default function App({ Component, pageProps }) {
+  // Render the ContextProvider component, wrapping the main component with the provided context
   return (
     <ContextProvider>
       <Component {...pageProps} />
