ea23219ea7880190284f6908f87b333081acb774
Wanton Raven <josephngalu96@gmail.com>
2023-06-13 11:35:20 +0300

    Update index.js

--- context/index.js
+++ context/index.js
@@ -3,15 +3,17 @@ import React, { useState, createContext } from "react";
 export const Context = createContext();
 
 export const ContextProvider = (props) => {
+  // State variables for username and secret
   const [username, setUsername] = useState("");
   const [secret, setSecret] = useState("");
 
+  // Create a context value object with the state variables and setter functions
   const value = {
     username,
     setUsername,
     secret,
     setSecret,
   };
-
+  // Return the Context.Provider component with the context value and child components
   return <Context.Provider value={value}>{props.children}</Context.Provider>;
 };
