f97d2192edc2f12ad187ea05b29beec2ad1d9a98
Wanton Raven <josephngalu96@gmail.com>
2023-06-13 11:46:17 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,14 +6,21 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+	    {/* favicon */}
           <link rel="icon" href="/img1.svg" className="custom-icon" />
+	    {/* image */}
           <link rel="preload" href="../images/img1.jpg" as="image" />
+	    {/* Preconnect to Google Fonts */}
 	  <link rel="preconnect" href="https://fonts.googleapis.com" />
+	    {/* Import a font from Google Fonts */}
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		  
           <link href="https://fonts.googleapis.com/css2?family=Geologica:wght@200&display=swap" rel="stylesheet" />
         </Head>
         <body>
+          {/* Render the main content */}
           <Main />
+          {/* Render the Next.js scripts */}
           <NextScript />
         </body>
       </Html>
